897085b112a45b3c014f1b1e76c39b8c05e29313
GopalDan <gopal125dan@gmail.com>
2022-02-02 20:26:08 +0530

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,16 +3,90 @@ import styled from "styled-components";
 function About() {
   return (
     <Wrapper>
-      <h1>About page</h1>
+      <div className="title">
+        <p className="about">ABOUT</p>
+        <p className="sri">SRI DURGA TVS</p>
+      </div>
+      <div className="container">
+        <p className="title-2">Sri Durga TVS, Bike for all.</p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quo
+          eveniet delectus, officia consequatur necessitatibus sunt totam
+          provident voluptas a ut soluta velit harum culpa id repellendus
+          doloribus, aspernatur fugit blanditiis voluptatum pariatur quisquam
+          obcaecati? Quo nulla non esse ullam quae odit debitis, voluptates vel
+          sapiente molestias doloribus dolor maiores perferendis sunt. Tempore
+          quaerat, laborum, quos nihil distinctio rem voluptate suscipit autem
+          eveniet ab iste aut? Laudantium esse repellat qui obcaecati nam. Illo,
+          eos. Error, architecto. Minus totam eius eum ab. Architecto officia,
+          dolor ea nobis fugit nostrum vel iste, animi sunt sed illo. Reiciendis
+          quo velit perferendis iusto dolore, similique odit asperiores maiores
+          dolor illum quae sunt, laborum eius culpa natus? Amet delectus ab
+          veniam, similique voluptate eaque enim. eos. odit asperiores maiores
+          <br></br>Error, architecto. Minus totam eius eum ab. Architecto
+          officia, dolor ea nobis fugit nostrum vel iste, animi sunt sed illo.
+          Reiciendis quo velit perferendis iusto dolore, similique odit
+          asperiores maiores dolor illum quae sunt, laborum eius culpa natus?
+          Amet delectus ab veniam, similique voluptate eaque enim.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tenetur
+          tempora facilis iure, delectus corporis culpa voluptatum aperiam amet
+          iste voluptatibus eveniet officiis officia beatae dicta aspernatur
+          recusandae autem fuga quos cumque quam id. Ex repellendus illo neque
+          odit culpa sint incidunt, magnam explicabo iure labore quo dolore
+          voluptas soluta! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Quod tenetur tempora facilis iure, delectus corporis culpa
+          voluptatum aperiam amet iste voluptatibus eveniet officiis officia
+          beatae dicta aspernatur recusandae autem fuga quos cumque quam id. Ex
+          repellendus illo neque odit culpa sint incidunt, magnam explicabo iure
+          labore quo dolore voluptas soluta!
+        </p>
+        <div className="divider"></div>
+      </div>
     </Wrapper>
   );
 }
 const Wrapper = styled.div`
-  width: 60vw;
-  /* min-height: 19vh; */
-  margin: 3rem auto;
-  h3 {
-    margin-top: 1rem;
+  .title {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background: #111;
+    padding: 3rem 0 5rem 0;
+    /* padding-left: 5vw; */
+  }
+
+  .about {
+    width: 90vw;
+    margin: 0 auto;
+    color: white;
+    font-size: 1.25rem;
+    font-weight: 300;
+  }
+  .sri {
+    width: 90vw;
+    margin: 0 auto;
+    color: white;
+    font-size: 1.25rem;
+    font-weight: 500;
+  }
+  .container {
+    width: 90vw;
+    margin: 0 auto;
+  }
+
+  .title-2 {
+    padding: 1rem 0;
+    color: #3a7ada;
+    font-weight: 500;
+    font-size: 1.25rem;
+  }
+  .divider {
+    border-bottom: 2px solid var(--subText50);
+    overflow: hidden;
+    margin: 1rem 0;
   }
 `;
 export default About;
